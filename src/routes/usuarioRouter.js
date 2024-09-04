8307a44428bccb24c9c258a5869075df9e03c718
Anthony Bruno <anthonybrunoprogramador@gmail.com>
2024-09-04 13:43:51 -0300

    toma raiane miseravel

---
+++ src/routes/usuarioRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { checkUser, login, register } from "../controllers/usuarioController.js"
+
+const router = Router()
+
+router.post("/register", register)
+router.post("/login", login)
+router.get("/checkuser", checkUser)
+
+
+export default router;
